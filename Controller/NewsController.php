4eaeffdba86a6ac31f3308fc2585b04e7609c863
I-eat-short-people <noturbear207@gmail.com>
2024-12-07 19:08:57 +0700

    them noi dung tep NewsController.php

--- Controller/NewsController.php
+++ Controller/NewsController.php
@@ -7,4 +7,47 @@ class NewsController {
         include "views/admin/news/index.php";
     }
 
-   
+    public function add() {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $title = $_POST['title'];
+            $content = $_POST['content'];
+            $image = $_FILES['image']['name'];
+            $category_id = $_POST['category'];
+
+            // Upload image
+            move_uploaded_file($_FILES['image']['tmp_name'], "uploads/" . $image);
+
+            News::add($title, $category_id, $content, $image);
+            header("Location: index.php?controller=news&action=index");
+        } else {
+            include "views/admin/news/add.php";
+        }
+    }
+
+    public function edit($id) {
+        $news = News::getById($id);
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $title = $_POST['title'];
+            $content = $_POST['content'];
+            $category_id = $_POST['category'];
+
+            // Check if a new image is uploaded
+            if ($_FILES['image']['name']) {
+                $image = $_FILES['image']['name'];
+                move_uploaded_file($_FILES['image']['tmp_name'], "uploads/" . $image);
+            } else {
+                $image = $news['image']; // Keep the old image
+            }
+
+            News::edit($id, $title, $category_id, $content, $image);
+            header("Location: index.php?controller=news&action=index");
+        }
+        include "views/admin/news/edit.php";
+    }
+
+    public function delete($id) {
+        News::delete($id);
+        header("Location: index.php?controller=news&action=index");
+    }
+}
+?>
